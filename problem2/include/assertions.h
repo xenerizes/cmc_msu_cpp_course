0c9233bb5353a2eda8a8d4dbbf828e35b27236c8
Yuliya <xenerizes@users.noreply.github.com>
2018-11-22 09:14:39 +0300

    Assertions for no exception

--- problem2/include/assertions.h
+++ problem2/include/assertions.h
@@ -3,12 +3,20 @@
 #include <exception>
 #include <cassert>
 
-#define STD_EXCEPTION_WRAPPER(cmd) auto _wrp = [&]() { \
+#define STD_EXCEPTION_CHECKER(cmd) auto _wrp = [&]() { \
     try { cmd; } \
     catch (const std::exception&) { return true; } \
     return false; \
 };
 
-#define ASSERT_STD_EXCEPTION(cmd) { STD_EXCEPTION_WRAPPER(cmd) \
+#define ANY_EXCEPTION_CHECKER(cmd) auto _wrp = [&]() { \
+    try { cmd; } \
+    catch (...) { return true; } \
+    return false; \
+};
+
+#define ASSERT_STD_EXCEPTION(cmd) { STD_EXCEPTION_CHECKER(cmd) \
     assert(_wrp()); }
 
+#define ASSERT_NO_EXCEPTION(cmd) { ANY_EXCEPTION_CHECKER(cmd) \
+    assert(!_wrp()); }
