7d89de581959d10ca8f495f73c6b8825c7238688
Yuliya <xenerizes@users.noreply.github.com>
2018-11-23 09:03:36 +0300

    Catch bad_any_cast

--- problem2/include/options.h
+++ problem2/include/options.h
@@ -17,7 +17,11 @@ struct TOptions {
         if (I >= options.size())
             throw std::out_of_range("No options at required index");
 
-        return std::any_cast<T>(options[I]);
+        try {
+            return std::any_cast<T>(options[I]);
+        } catch (const std::bad_any_cast&) {
+            throw std::domain_error("Option at required index has different type");
+        }
     };
 
     std::vector<std::any> options;
