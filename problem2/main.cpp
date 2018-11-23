fb66650b77a9b515677a89841a1724ae17ec9a2c
Yuliya <xenerizes@users.noreply.github.com>
2018-11-23 11:43:02 +0300

    Tests for finite and geometric distributions

--- problem2/main.cpp
+++ problem2/main.cpp
@@ -5,6 +5,8 @@
 #include "distribution_factory.h"
 #include "assertions.h"
 
+using TVec = TFiniteGenerator::TVec;
+
 int main()
 {
     TRandomGeneratorFactory gfactory;
@@ -12,5 +14,10 @@ int main()
     ASSERT_NO_EXCEPTION(gfactory.CreateGenerator("bernoulli", 0.5));
     ASSERT_STD_EXCEPTION(gfactory.CreateGenerator("poisson", -15.5));
     ASSERT_NO_EXCEPTION(gfactory.CreateGenerator("poisson", 2.0));
+    ASSERT_STD_EXCEPTION(gfactory.CreateGenerator("geometric", 15.5));
+    ASSERT_NO_EXCEPTION(gfactory.CreateGenerator("geometric", 0.3));
+    ASSERT_STD_EXCEPTION(gfactory.CreateGenerator("finite", TVec{0.1}, TVec{1.1}));
+    ASSERT_STD_EXCEPTION(gfactory.CreateGenerator("finite", TVec{0.1}, TVec{0.1, 1.0}));
+    ASSERT_NO_EXCEPTION(gfactory.CreateGenerator("finite", TVec{54, 11}, TVec{0.1, 0.2}));
     return 0;
 }
