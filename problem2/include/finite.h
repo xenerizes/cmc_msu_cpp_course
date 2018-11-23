0c7e9ca5548d60389fdfe48cbf8cd3fc50c589ce
Yuliya <xenerizes@users.noreply.github.com>
2018-11-23 11:00:55 +0300

    Finite distribution impl

--- problem2/include/finite.h
+++ problem2/include/finite.h
@@ -1,7 +1,23 @@
 #pragma once
 
 #include "generator_base.h"
+#include "options.h"
+#include "utility.h"
+#include <vector>
 
 class TFiniteGenerator
     : public IRandomNumberGenerator
-{};
+{
+public:
+    using TVec = std::vector<double>;
+
+    TFiniteGenerator(const TVec& values, const TVec& probas);
+    TFiniteGenerator(const TOptions& opts);
+
+    double Generate() const override;
+
+private:
+    TVec values;
+    TVec probas;
+    utility::distribution_holder<std::discrete_distribution<>> impl;
+};
