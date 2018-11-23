f3d8aae707801561d5548aed9f4fc625221aa12c
Yuliya <xenerizes@users.noreply.github.com>
2018-11-23 11:42:46 +0300

    Register remaining distributions

--- problem2/src/distribution_factory.cpp
+++ problem2/src/distribution_factory.cpp
@@ -1,6 +1,8 @@
 #include "distribution_factory.h"
 #include <bernoulli.h>
 #include <poisson.h>
+#include <geometric.h>
+#include <finite.h>
 
 TRandomGeneratorFactory::TRandomGeneratorFactory()
 {
@@ -11,4 +13,6 @@ void TRandomGeneratorFactory::RegisterAll()
 {
     Register<TBernoulliGenerator>("bernoulli");
     Register<TPoissonGenerator>("poisson");
+    Register<TGeometricGenerator>("geometric");
+    Register<TFiniteGenerator>("finite");
 }
