7bb3cd83e6f220d07bf9245311b5131fae682046
Yuliya <xenerizes@users.noreply.github.com>
2018-12-22 22:35:24 +0300

    Change price test, one Auchan

--- problem3/test/InteractionTest.h
+++ problem3/test/InteractionTest.h
@@ -29,8 +29,6 @@ TEST(OneAuchan, BuyTest) {
         b.set_value();
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
-
-        //ASSERT_TRUE(shop.use_count() == 1);
     }, std::ref(p), std::ref(barrier));
 
     auto shop = std::make_shared< Auchan >();
@@ -42,6 +40,47 @@ TEST(OneAuchan, BuyTest) {
 
     ASSERT_DOUBLE_EQ(58, shop->Buy("condensed_milk"));
     ASSERT_DOUBLE_EQ(0, shop->Buy("green_bag"));
+
+    product_thread.join();
+}
+
+TEST(OneAuchan, ChangePriceTest) {
+    std::promise< IShopPtr > p;
+    std::promise< void > barrier;
+
+    std::thread product_thread([](std::promise< IShopPtr >& p,
+                                  std::promise< void >& b) {
+        auto shop_future = p.get_future();
+        shop_future.wait();
+        auto shop = shop_future.get();
+
+        CondensedMilk p1(58);
+        GreenBag p2(1.5);
+        p1.Attach(shop);
+        p2.Attach(shop);
+        p1.StartSales();
+        p2.StartSales();
+
+        b.set_value();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        p1.ChangePrice(42);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }, std::ref(p), std::ref(barrier));
+
+    auto shop = std::make_shared< Auchan >();
+    p.set_value(shop);
+
+    auto barrier_future = barrier.get_future();
+    barrier_future.wait();
+
+    ASSERT_DOUBLE_EQ(58, shop->Buy("condensed_milk"));
+    ASSERT_DOUBLE_EQ(1.5, shop->Buy("green_bag"));
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    ASSERT_DOUBLE_EQ(42, shop->Buy("condensed_milk"));
+    ASSERT_DOUBLE_EQ(1.5, shop->Buy("green_bag"));
 
     product_thread.join();
 }
